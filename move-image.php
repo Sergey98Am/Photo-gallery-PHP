f00969c48daacdd572c4473f5ab8ce37ad64f330
Sergey98Am <serg98barca@gmail.com>
2021-11-01 16:27:16 +0400

    some changes

--- move-image.php
+++ move-image.php
@@ -3,13 +3,10 @@
 if (isset($_POST['move'])) {
     $file_path = $_POST['file_path'];
     $destination_file_path = $_POST['destination_file_path'];
-    if ($file_path || $destination_file_path) {
+    if (empty($file_path) || empty($destination_file_path)) {
         echo "file_path or destination_file_path are empty";
     } else {
-        if (!rename($file_path, $destination_file_path)) {
-            echo "File can't be moved!";
-        } else {
-            header("Location: index.php");
-        }
+        rename($file_path, $destination_file_path);
+        header("Location: index.php");
     }
 }
